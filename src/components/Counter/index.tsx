6fb33b6a9be826945c954480d00af6bc6d55e7f2
WellintonDhionys <wellintondhionys@gmail.com>
2023-01-02 15:33:20 -0400

    feat: count component

---
+++ src/components/Counter/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+export default function Counter() {
+    const [count, setCount] = useState(1)
+    const [disabled, setDisabled] = useState(false)
+
+    const increment = () => {
+        setCount(current => current + 1)
+    }
+
+    const decrement = () => {
+        setCount(current => current - 1)
+    }
+
+    useEffect(() => {
+        setDisabled(false)
+        if (count <= 0) setDisabled(true)
+    }, [count])
+
+    return (
+        <div className="flex flex-wrap items-center justify-center w-1/3 bg-[#fff]">
+            <button onClick={decrement} disabled={disabled} className="w-8 h-8 font-semibold text-[#000] border border-[#000]">-</button>
+            <input className="w-8 h-8 outline-none text-center text-sm border border-y-[#000]" value={count} />
+            <button onClick={increment} className="w-8 h-8 font-semibold text-[#000] border border-[#000]">+</button>
+        </div>
+    )
+}
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import Counter from '../../components/Counter';
 
 const products = [
   {
@@ -25,29 +26,33 @@ const products = [
 function Cart() {
   return (
     <div className="max-w-[1280px] mx-auto flex flex-col items-center my-20">
-      {
-        products && products.map(product => (
-        <div className="w-[900px] overflow-hidden shadow-md bg-white px-6 py-4">
-          <div className="flex flex-row w-full my-6">
-            <div className="flex justify-center w-1/4 mr-6">
-              <img className="w-56" src={product?.url} alt="Sunset in the mountains" />
-            </div>
-            <div className='flex flex-wrap w-1/2'>
-              <div className="max-w-[300px]">
-                <p className='text-slate-500 text-sm'>{product?.description}</p>
-                <p className='font-bold text-[#17cc11] text-sm'>R$ {product?.value}</p>
+      <h1 className='text-2xl text-white font-black mt-1'>Cart</h1>
+      <div className='p-4'>
+        {
+          products && products.map(product => (
+          <div className="w-[900px] overflow-hidden shadow-md bg-white px-6 py-4">
+            <div className="flex flex-row w-full my-6">
+              <div className="flex justify-center w-1/4 mr-6">
+                <img className="w-56" src={product?.url} alt="Sunset in the mountains" />
+              </div>
+              <div className='flex flex-wrap w-1/4'>
+                <div className="max-w-[300px]">
+                  <p className='text-slate-500 text-sm'>{product?.description}</p>
+                  <p className='font-bold text-[#17cc11] text-sm'>R$ {product?.value}</p>
+                </div>
+              </div>
+              <Counter />
+              <div className="flex flex-wrap items-center justify-cente w-1/12">
+                <p className='font-bold text-xs'>R$ {product?.value}</p>
+              </div>
+              <div className="flex flex-wrap items-center justify-end w-1/12">
+                <button className="w-8 h-8"><FontAwesomeIcon icon={faTrash} color="#cc1111" /></button>
               </div>
             </div>
-            <div className="flex flex-wrap items-center justify-cente w-1/12">
-              <p className='font-bold text-xs'>R$ {product?.value}</p>
-            </div>
-            <div className="flex flex-wrap items-center justify-end w-1/12">
-              <button className="w-8 h-8"><FontAwesomeIcon icon={faTrash} color="#cc1111" /></button>
-            </div>
           </div>
-        </div>
-        ))
-      }
+          ))
+        }
+      </div>
     </div>
   );
 }
